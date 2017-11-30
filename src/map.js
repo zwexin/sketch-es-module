b21ddf6b2ee95564c5491179c3349f72bc3eec72
zzswang <zzswang@gmail.com>
2017-11-30 22:20:10 +0800

    feat: flow annotation

--- src/map.js
+++ src/map.js
@@ -1,4 +1,6 @@
-export function map([head, ...tail], fn) {
+// @flow
+
+export function map([head, ...tail]: Array<any>, fn: any => any) {
   if (head === undefined && !tail.length) return [];
   return tail.length ? [fn(head), ...map(tail, fn)] : [fn(head)];
 }
